62845663897f9093ccc76fa3f47f82af68fde68f
Mary-Kate James <marykatj@gmail.com>
2018-06-06 02:13:08 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
-class Dog
-  def bark
-    puts "Woof!"
+class Person
+  def talk
+    puts "Hello World"
   end
 
-  def sit
-    puts "The Dog is sitting"
+  def walk
+    puts "The Person is walking"
   end
 
 end
